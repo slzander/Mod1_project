f14d6d730f8cddc6460e9b2030a9ff078e42fd47
slzander <staceyzander@gmail.com>
2019-10-24 11:50:17 -0600

    add_a_word_method

--- config/environment.rb
+++ config/environment.rb
@@ -10,4 +10,50 @@ ActiveRecord::Base.logger = nil
 
 
 
+def play_or_add_word
+    prompt = TTY::Prompt.new
+    response = prompt.select("      Would you like to play or add a new word to the game?", 
+    ["       Play", "       Add a new word"])
+    if response == "       Play"
+        difficulty
+    else
+        add_word_to_game
+    end
+end
+
+def difficulty
+    system('clear')
+    puts chompman_title
+    #  ... etc ...
+end
+
+
+def add_word_to_game
+    system('clear')
+    # puts chompman_title
+    puts "      Type the word you would like to add:"
+    puts ""
+    new_word = gets.chomp
+    puts ""
+    puts "      Type the definition for this word:"
+    puts ""
+    new_definition = gets.chomp
+    puts ""
+    new_difficulty = prompt.select("      Rate the difficulty of the word:", ["       Easy", "       Medium", "       Hard"])
+    if response == "       Easy"
+        new_difficulty = 1
+    elsif response == "       Medium"
+        new_difficulty = 2
+    else
+        new_difficulty = 3
+    end
+    SecretWord.create(word: new_word, hint: new_definition, difficulty: new_difficulty)
+    puts ""
+    puts "      Thanks for adding a new word to the game!"
+    play_or_add_word
+end
+
+
+
+
 # binding.pry
--- runner.rb
+++ runner.rb
@@ -5,7 +5,4 @@ new_cli = Cli.new(nil, nil)
 new_cli.start
 
 
-# binding.pry
-
-
 
